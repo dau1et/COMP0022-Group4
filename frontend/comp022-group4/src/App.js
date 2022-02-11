73091d1738a08dd5059be808e3a393809a1f16fb
Michael <michael.khot@hotmail.co.uk>
2022-02-11 13:57:17 +0000

    Create react app

---
+++ frontend/comp022-group4/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>COMP022 Group4</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
