0797c473389dce49dc97eb47a209b15fc3c14fef
Daulet Batayev <batayev.d@gmail.com>
2022-02-14 17:40:06 +0000

    update schema and script for populating the db

---
+++ dev/db/database.sql
@@ -0,0 +1,85 @@
+CREATE TABLE language(
+  ISO639_1 VARCHAR(2) PRIMARY KEY,
+  language_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE movie (
+  movie_id BIGINT PRIMARY KEY UNIQUE,
+  title VARCHAR(255) NOT NULL,
+  overview VARCHAR(1000),
+  runtime INT, -- lets keep it Timestamp atm, we can later change it to something else in the future
+  popularity float8,
+  adult BOOLEAN,
+  status VARCHAR(50),
+  release_date TIMESTAMP, -- same, might chage the datatype later
+  budget BIGINT,
+  revenue BIGINT,
+  ISO639_1 VARCHAR(2) REFERENCES language(ISO639_1), -- can we use a dash in the field name?
+  poster_path VARCHAR(100),
+  backdrop_path VARCHAR(100)
+);
+
+CREATE TABLE movieTag (
+  movie_id BIGINT REFERENCES movie(movie_id) NOT NULL,
+  tag VARCHAR(100) NOT NULL,
+  PRIMARY KEY(movie_id, tag)
+);
+
+CREATE TABLE movieTmdb(
+  movie_id BIGINT REFERENCES movie(movie_id) PRIMARY KEY,
+  tmdb_id INT
+);
+
+CREATE TABLE movieTranslation(
+  movie_id BIGINT REFERENCES movie(movie_id) NOT NULL,
+  ISO639_1 VARCHAR(2) REFERENCES language(ISO639_1) NOT NULL,
+  PRIMARY KEY(movie_id, ISO639_1)
+);
+
+CREATE TABLE personalityData(
+  user_id VARCHAR(100) PRIMARY KEY,
+  openness float8 NOT NULL,
+  agreeableness float8 NOT NULL,
+  emotional_stability float8 NOT NULL,
+  consientiousness float8 NOT NULL,
+  extraversion float8 NOT NULL
+);
+
+CREATE TABLE rating(
+  user_id VARCHAR(100) REFERENCES personalityData(user_id) NOT NULL,
+  movie_id BIGINT REFERENCES movie(movie_id) NOT NULL,
+  rating float8 NOT NULL,
+  timestamp TIMESTAMP NOT NULL,
+  PRIMARY KEY(user_id, movie_id)
+);
+
+CREATE TABLE actor(
+  actor_id INT PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50)
+);
+
+CREATE TABLE movieActor(
+  movie_id BIGINT REFERENCES movie(movie_id) NOT NULL,
+  actor_id INT REFERENCES actor(actor_id) NOT NULL,
+  PRIMARY KEY(movie_id, actor_id)
+);
+
+CREATE TABLE publisher(
+  publisher_id INT PRIMARY KEY,
+  publishing_company VARCHAR(100) NOT NULL,
+  publishing_country VARCHAR(100)
+);
+
+CREATE TABLE moviePublisher(
+  movie_id BIGINT REFERENCES movie(movie_id) NOT NULL,
+  publisher_id INT REFERENCES publisher(publisher_id) NOT NULL,
+  PRIMARY KEY(movie_id, publisher_id)
+);
+
+CREATE TABLE movieGenre(
+  movie_id BIGINT REFERENCES movie(movie_id) NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  PRIMARY KEY(movie_id, genre)
+);
+
---
+++ dev/populate_db.py
@@ -0,0 +1,49 @@
+import psycopg2
+import csv
+import regex as re
+
+def cast_check(val):
+    if not val:
+        return None  # account for the missing fields for a movie, and fill those fields with "NULL"
+
+    if val.isdigit():
+        return int(val)
+
+    if re.match(r'^-?\d+(?:\.\d+)$', val):  # check if str representation is a float
+        return float(val)
+
+    return val
+
+def populate_table(table_name, field_count):
+
+    with open(f"../csv_data/{table_name}.csv", newline = '') as csvfile:
+        placeholder = "%s, "
+        reader = csv.reader(csvfile, delimiter = ',')
+        for row in reader:
+            fields = [cast_check(field) for field in row]
+            cur.execute(f"INSERT INTO {table_name} VALUES " + f"({placeholder * (field_count - 1)}%s)", fields)
+
+
+if __name__ == "__main__":
+    connection = psycopg2.connect(
+                host = "localhost",
+                database = "moviedb",
+                user = "moviedb-dev",
+                password = "moviedb-dev",
+                port = 5432
+            )
+
+    cur = connection.cursor()
+
+    # a list of (table_name, field_count) tuples
+    tables = [ ("Language", 2), ("Movie", 13), ("MovieTmdb", 2), ("Actor", 3), ("MovieActor", 2), ("MovieGenre", 2), ("Publisher", 3), ("MoviePublisher", 2), ("PersonalityData", 6), ("MovieTag", 2), ("Rating", 4), ("MovieTranslation", 2)]
+
+#    tables = [ ("Language", 2), ("Movie", 12), ("MovieTmdb", 2), ("Actor", 3), ("MovieActor", 2), ("MovieGenre", 2), ("Publisher", 3), ("MoviePublisher", 2)]
+
+    for table_name, field_count in tables:
+        populate_table(table_name, field_count)
+        print(table_name)
+
+    connection.commit()
+    cur.close()
+    connection.close()
