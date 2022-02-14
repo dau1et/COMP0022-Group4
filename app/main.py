5836b3173aab66a0dd51c6810b2362fcd9954530
Hamza Nasir <hamzanasir@hotmail.co.uk>
2022-02-14 02:43:06 +0000

    Add blank API endpoints

--- app/main.py
+++ app/main.py
@@ -6,3 +6,44 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# TODO: Add query params
+@app.get("/api/movies")
+async def get_movies():
+    return []
+
+
+@app.get("/api/movies/{movie_id}")
+async def get_movie(movie_id: int):
+    return {"movie_id": movie_id}
+
+
+@app.get("/api/movies/{movie_id}/actors")
+async def get_movie_actors(movie_id: int):
+    return []
+
+
+@app.get("/api/movies/{movie_id}/publishers")
+async def get_movie_publishers(movie_id: int):
+    return []
+
+
+@app.get("/api/movies/{movie_id}/genres")
+async def get_movie_genres(movie_id: int):
+    return []
+
+
+@app.get("/api/movies/{movie_id}/tags")
+async def get_movie_tags(movie_id: int):
+    return []
+
+
+@app.get("/api/movies/{movie_id}/translations")
+async def get_movie_translations(movie_id: int):
+    return []
+
+
+@app.get("/api/movies/{movie_id}/ratings")
+async def get_movie_ratings(movie_id: int):
+    return []
