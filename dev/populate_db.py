29075e678037ea689f5334ecf22d98fe355360ab
Hamza Nasir <hamzanasir@hotmail.co.uk>
2022-02-16 15:32:49 +0000

    Update populate_db.py

--- dev/populate_db.py
+++ dev/populate_db.py
@@ -1,28 +1,24 @@
 import psycopg2
 import csv
-import regex as re
 
 def cast_check(val):
     if not val:
         return None  # account for the missing fields for a movie, and fill those fields with "NULL"
 
-    if val.isdigit():
-        return int(val)
-
-    if re.match(r'^-?\d+(?:\.\d+)$', val):  # check if str representation is a float
-        return float(val)
+    try:
+        val = float(val)
+    except ValueError:
+        pass
 
     return val
 
 def populate_table(table_name, field_count):
 
-    with open(f"../csv_data/{table_name}.csv", newline = '') as csvfile:
-        placeholder = "%s, "
-        reader = csv.reader(csvfile, delimiter = ',')
+    with open(f"../csv_data/{table_name}.csv", newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             fields = [cast_check(field) for field in row]
-            cur.execute(f"INSERT INTO {table_name} VALUES " + f"({placeholder * (field_count - 1)}%s)", fields)
-
+            cur.execute(f"INSERT INTO {table_name} VALUES " + f"({', '.join('%s' for _ in range(field_count))})", fields)
 
 if __name__ == "__main__":
     connection = psycopg2.connect(
@@ -36,7 +32,7 @@ if __name__ == "__main__":
     cur = connection.cursor()
 
     # a list of (table_name, field_count) tuples
-    tables = [ ("Language", 2), ("Movie", 13), ("MovieTmdb", 2), ("Actor", 3), ("MovieActor", 2), ("MovieGenre", 2), ("Publisher", 3), ("MoviePublisher", 2), ("PersonalityData", 6), ("MovieTag", 2), ("Rating", 4), ("MovieTranslation", 2)]
+    tables = [("Language", 2), ("Movie", 13), ("MovieTmdb", 2), ("Actor", 3), ("MovieActor", 2), ("MovieGenre", 2), ("Publisher", 3), ("MoviePublisher", 2), ("PersonalityData", 6), ("MovieTag", 2), ("Rating", 4), ("MovieTranslation", 2)]
 
     for table_name, field_count in tables:
         populate_table(table_name, field_count)
