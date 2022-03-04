d52caccb6bf0bdc178a446d29162ea05480b73a5
Hamza Nasir <hamzanasir@hotmail.co.uk>
2022-03-04 14:49:44 +0000

    Add database queries for API endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,38 @@
+from pickletools import read_uint1
+from typing import Literal
+
 import asyncpg
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Query, Request
+
+from app.query import QueryBuilder
 
 app = FastAPI()
+
+SortDirection = Literal["ASC", "DESC"]
+
+SortFields = Literal[
+    "title",
+    "release_date",
+    "popularity",
+    "runtime",
+    "revenue",
+    "budget"
+]
+
+MOVIE_COLUMNS = [
+    "title",
+    "overview",
+    "runtime",
+    "popularity",
+    "adult",
+    "status",
+    "release_date",
+    "budget",
+    "revenue",
+    "iso639_1",
+    "poster_path",
+    "backdrop_path"
+]
 
 
 @app.on_event("startup")
@@ -25,20 +56,30 @@ async def root():
     return {"message": "Hello World"}
 
 
-# TODO: Filter by query params
 @app.get("/api/movies")
 async def get_movies(request: Request, runtime_min: int | None = None, runtime_max: int | None = None, 
-                        popularity_min: float | None = None, popularity_max: float | None = None,
-                        adult: bool | None = None):
+                        revenue_min: float | None = None, revenue_max: float | None = None,
+                        genres: list[str] | None = Query(None), language: str | None = None,
+                        sort_by: SortFields = "title", sort_direction: SortDirection = "ASC"):
+    query_builder = QueryBuilder(MOVIE_COLUMNS, "Movie", "movie_id")
+    query_builder.add_range_filter("runtime", runtime_min, runtime_max)
+    query_builder.add_range_filter("revenue", revenue_min, revenue_max)
+    if language is not None:
+        query_builder.add_equality_filter("iso639_1", language)
+    if genres is not None:
+        query_builder.add_membership_filter("genre", genres, from_table="MovieGenre")
+    query_builder.add_order_by(sort_by, sort_direction)
+    query, args = query_builder.build()
+
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT title, overview, runtime, popularity, adult, status, release_date, budget, revenue, iso639_1, poster_path, backdrop_path FROM Movie")
+        records = await conn.fetch(query, *args)
     return records
 
 
 @app.get("/api/movies/{movie_id}")
 async def get_movie(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        record = await conn.fetchrow(f"SELECT title, overview, runtime, popularity, adult, status, release_date, budget, revenue, iso639_1, poster_path, backdrop_path FROM Movie WHERE movie_id={movie_id}")
+        record = await conn.fetchrow(f"SELECT title, overview, runtime, popularity, adult, status, release_date, budget, revenue, iso639_1, poster_path, backdrop_path FROM Movie WHERE movie_id=$1", movie_id)
     if record is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return record
@@ -47,40 +88,40 @@ async def get_movie(request: Request, movie_id: int):
 @app.get("/api/movies/{movie_id}/actors")
 async def get_movie_actors(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT Actor.first_name, Actor.last_name FROM Actor INNER JOIN MovieActor ON Actor.actor_id=MovieActor.actor_id AND MovieActor.movie_id={movie_id}")
+        records = await conn.fetch(f"SELECT Actor.first_name, Actor.last_name FROM Actor INNER JOIN MovieActor ON Actor.actor_id=MovieActor.actor_id AND MovieActor.movie_id=$1", movie_id)
     return records
 
 
 @app.get("/api/movies/{movie_id}/publishers")
 async def get_movie_publishers(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT Publisher.publishing_company, Publisher.publishing_country FROM Publisher INNER JOIN MoviePublisher ON Publisher.publisher_id=MoviePublisher.publisher_id AND MoviePublisher.movie_id={movie_id}")
+        records = await conn.fetch(f"SELECT Publisher.publishing_company, Publisher.publishing_country FROM Publisher INNER JOIN MoviePublisher ON Publisher.publisher_id=MoviePublisher.publisher_id AND MoviePublisher.movie_id=$1", movie_id)
     return records
 
 
 @app.get("/api/movies/{movie_id}/genres")
 async def get_movie_genres(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT genre FROM MovieGenre WHERE movie_id={movie_id}")
+        records = await conn.fetch(f"SELECT genre FROM MovieGenre WHERE movie_id=$1", movie_id)
     return records
 
 
 @app.get("/api/movies/{movie_id}/tags")
 async def get_movie_tags(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT tag FROM MovieTag WHERE movie_id={movie_id}")
+        records = await conn.fetch(f"SELECT tag FROM MovieTag WHERE movie_id=$1", movie_id)
     return records
 
 
 @app.get("/api/movies/{movie_id}/translations")
 async def get_movie_translations(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT Language.iso639_1, Language.language_name FROM Language INNER JOIN MovieTranslation ON Language.iso639_1=MovieTranslation.iso639_1 AND MovieTranslation.movie_id={movie_id}")
+        records = await conn.fetch(f"SELECT Language.iso639_1, Language.language_name FROM Language INNER JOIN MovieTranslation ON Language.iso639_1=MovieTranslation.iso639_1 AND MovieTranslation.movie_id=$1", movie_id)
     return records
 
 
 @app.get("/api/movies/{movie_id}/ratings")
 async def get_movie_ratings(request: Request, movie_id: int):
     async with request.app.state.conn_pool.acquire() as conn:
-        records = await conn.fetch(f"SELECT user_id, rating, timestamp FROM Rating WHERE movie_id={movie_id}")
+        records = await conn.fetch(f"SELECT user_id, rating, timestamp FROM Rating WHERE movie_id=$1", movie_id)
     return records
---
+++ app/query.py
@@ -0,0 +1,53 @@
+from typing import Any, Literal
+
+SortDirection = Literal["ASC", "DESC"]
+
+class QueryBuilder:
+    def __init__(self, columns: list[str], table: str, id_field: str) -> None:
+        self._id_field = id_field
+        self._query = None
+        self._clauses = [f"SELECT {', '.join(columns)} FROM {table}"]
+        self._filters = []
+        self._order_by = None
+
+        self._args = []
+        self._arg_num = 1
+    
+    def add_equality_filter(self, field_name: str, value: Any) -> None:
+        self._filters.append(f"{field_name} = ${self._arg_num}")
+        self._args.append(value)
+        self._arg_num += 1
+    
+    def add_range_filter(self, field_name: str, field_min: Any, field_max: Any) -> None:
+        if field_min is not None and field_max is not None:
+            self._filters.append(f"{field_name} BETWEEN ${self._arg_num} and ${self._arg_num + 1}")
+            self._args.append(field_min)
+            self._args.append(field_max)
+            self._arg_num += 2
+        elif field_min is not None:
+            self._filters.append(f"{field_name} >= ${self._arg_num}")
+            self._args.append(field_min)
+            self._arg_num += 1
+        elif field_max is not None:
+            self._filters.append(f"{field_name} <= ${self._arg_num}")
+            self._args.append(field_max)
+            self._arg_num += 1
+    
+    def add_membership_filter(self, field_name: str, values: list[Any], from_table: str | None = None) -> None:
+        arg_list = [f"${self._arg_num + i}" for i in range(len(values))]
+        if from_table is None:
+            self._filters.append(f"{field_name} IN ({', '.join(arg_list)})")
+        else:
+            self._filters.append(f"{self._id_field} IN (SELECT {self._id_field} FROM {from_table} WHERE {field_name} IN ({', '.join(arg_list)}))")
+        self._args.extend(values)
+        self._arg_num += len(values)
+    
+    def add_order_by(self, field: str, direction: SortDirection) -> None:
+        self._order_by = f"ORDER BY {field} {direction}"
+    
+    def build(self) -> tuple[str, list[Any]]:
+        if self._filters:
+            self._clauses.append("WHERE " + " AND ".join(self._filters))
+        if self._order_by is not None:
+            self._clauses.append(self._order_by)
+        return " ".join(self._clauses), self._args
